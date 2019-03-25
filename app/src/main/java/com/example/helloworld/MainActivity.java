3d7eba926e02b7104a5ad8ed67615a329081b665
linyu0119 <ly15080059171>
2019-03-25 18:08:02 +0800

    1

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivityLife","调用onCreate()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivityLife","调用onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivityLife","调用onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivityLife","调用onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("MainActivityLife","调用onStop()");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivityLife","调用onDestroy()");
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.i("MainActivityLife","调用onRestart()");
     }
 }
